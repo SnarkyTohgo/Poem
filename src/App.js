4ddb2dc36eda45f47503a28b585be076cf1538e3
Tohgo Dreams <design.tohgo@gmail.com>
2020-07-26 20:58:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="main-quote">You are the pulsing of my heart</h1>
+      <div className="heart" id="heart">
+        <span>Adriana</span>
+      </div>
+
+      <blockquote className="poem">
+        It was in the stars you know,<br/>
+        Me becoming one with you.<br/>
+        I would wish on them everyday<br/>
+        And my wishes have come true.<br/>
+
+        You 're more than just my lover,<br/>
+        You 're my very best friend.<br/>
+        I know that I will hold your hand<br/>
+        Until our time on Earth ends.<br/>
+
+        Honey, did I tell you yet today<br/>
+        How much you mean to me ?<br/>
+        For you my perfect match,<br/>
+        I would swim the deepest sea.<br/>
+
+        I would climb the highest mountain<br/>
+        If you were stuck on top.<br/>
+        Kiss me once again my dear,<br/>
+        And please never ever stop.<br/>
+      </blockquote>
+    </>
   );
 }
+
 
 export default App;
